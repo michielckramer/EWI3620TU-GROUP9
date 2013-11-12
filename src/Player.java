acc1941245f77e2902b8b163f37327ad279244d2
michielckramer <michielkramer@hotmail.com>
2013-11-12 10:10:00 +0100

    Initial setup

---
+++ src/Player.java
@@ -0,0 +1,174 @@
+/**
+ * Player represents the actual player in MazeRunner.
+ * <p>
+ * This class extends GameObject to take advantage of the already implemented
+ * location functionality. Furthermore, it also contains the orientation of the
+ * Player, ie. where it is looking at and the player's speed.
+ * <p>
+ * For the player to move, a reference to a Control object can be set, which can
+ * then be polled directly for the most recent input.
+ * <p>
+ * All these variables can be adjusted freely by MazeRunner. They could be
+ * accessed by other classes if you pass a reference to them, but this should be
+ * done with caution.
+ * 
+ * @author Bruno Scheele
+ * 
+ */
+public class Player extends GameObject {
+	private double horAngle, verAngle;
+	private double speed;
+
+	private Control control = null;
+
+	/**
+	 * The Player constructor.
+	 * <p>
+	 * This is the constructor that should be used when creating a Player. It
+	 * sets the starting location and orientation.
+	 * <p>
+	 * Note that the starting location should be somewhere within the maze of
+	 * MazeRunner, though this is not enforced by any means.
+	 * 
+	 * @param x
+	 *            the x-coordinate of the location
+	 * @param y
+	 *            the y-coordinate of the location
+	 * @param z
+	 *            the z-coordinate of the location
+	 * @param h
+	 *            the horizontal angle of the orientation in degrees
+	 * @param v
+	 *            the vertical angle of the orientation in degrees
+	 */
+	public Player(double x, double y, double z, double h, double v) {
+		// Set the initial position and viewing direction of the player.
+		super(x, y, z);
+		horAngle = h;
+		verAngle = v;
+		speed = 0.01;
+	}
+
+	/**
+	 * Sets the Control object that will control the player's motion
+	 * <p>
+	 * The control must be set if the object should be moved.
+	 * 
+	 * @param input
+	 */
+	public void setControl(Control control) {
+		this.control = control;
+	}
+
+	/**
+	 * Gets the Control object currently controlling the player
+	 * 
+	 * @return
+	 */
+	public Control getControl() {
+		return control;
+	}
+
+	/**
+	 * Returns the horizontal angle of the orientation.
+	 * 
+	 * @return the horAngle
+	 */
+	public double getHorAngle() {
+		return horAngle;
+	}
+
+	/**
+	 * Sets the horizontal angle of the orientation.
+	 * 
+	 * @param horAngle
+	 *            the horAngle to set
+	 */
+	public void setHorAngle(double horAngle) {
+		this.horAngle = horAngle;
+	}
+
+	/**
+	 * Returns the vertical angle of the orientation.
+	 * 
+	 * @return the verAngle
+	 */
+	public double getVerAngle() {
+		return verAngle;
+	}
+
+	/**
+	 * Sets the vertical angle of the orientation.
+	 * 
+	 * @param verAngle
+	 *            the verAngle to set
+	 */
+	public void setVerAngle(double verAngle) {
+		this.verAngle = verAngle;
+	}
+
+	/**
+	 * Returns the speed.
+	 * 
+	 * @return the speed
+	 */
+	public double getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * Sets the speed.
+	 * 
+	 * @param speed
+	 *            the speed to set
+	 */
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	/**
+	 * Updates the physical location and orientation of the player
+	 * 
+	 * @param deltaTime
+	 *            The time in milliseconds since the last update.
+	 */
+	public void update(int deltaTime) {
+		if (control != null) {
+			control.update();
+			setHorAngle((double) control.getdX());
+			setVerAngle((double) control.getdY());
+			if (control.getForward()) {
+				this.setLocationX(locationX
+						- Math.sin(Math.toRadians(getHorAngle())) * deltaTime
+						* speed);
+				this.setLocationZ(locationZ
+						- Math.cos(Math.toRadians(getHorAngle())) * deltaTime
+						* speed);
+			}
+			if (control.getBack()) {
+				this.setLocationX(locationX
+						- Math.sin(Math.toRadians(getHorAngle() + 180))
+						* deltaTime * speed);
+				this.setLocationZ(locationZ
+						- Math.cos(Math.toRadians(getHorAngle() + 180))
+						* deltaTime * speed);
+			}
+			if (control.getLeft()) {
+				this.setLocationX(locationX
+						- Math.sin(Math.toRadians(getHorAngle() + 90))
+						* deltaTime * speed);
+				this.setLocationZ(locationZ
+						- Math.cos(Math.toRadians(getHorAngle() + 90))
+						* deltaTime * speed);
+			}
+			if (control.getRight()) {
+				this.setLocationX(locationX
+						- Math.sin(Math.toRadians(getHorAngle() - 90))
+						* deltaTime * speed);
+				this.setLocationZ(locationZ
+						- Math.cos(Math.toRadians(getHorAngle() - 90))
+						* deltaTime * speed);
+			}
+		}
+	}
+}
---
+++ src/UserInput.java
@@ -0,0 +1,145 @@
+import java.awt.event.*;
+
+import javax.media.opengl.GLCanvas;
+
+/**
+ * The UserInput class is an extension of the Control class. It also implements
+ * three interfaces, each providing handler methods for the different kinds of
+ * user input.
+ * <p>
+ * For making the assignment, only some of these handler methods are needed for
+ * the desired functionality. The rest can effectively be left empty (i.e. the
+ * methods under 'Unused event handlers').
+ * <p>
+ * Note: because of how java is designed, it is not possible for the game window
+ * to react to user input if it does not have focus. The user must first click
+ * the window (or alt-tab or something) before further events, such as keyboard
+ * presses, will function.
+ * 
+ * @author Mattijs Driel
+ * 
+ */
+public class UserInput extends Control implements MouseListener,
+		MouseMotionListener, KeyListener {
+	int x, y, sx, sy;
+	int dx = -90;
+	int dy = 0;
+
+	/**
+	 * UserInput constructor.
+	 * <p>
+	 * To make the new UserInput instance able to receive input, listeners need
+	 * to be added to a GLCanvas.
+	 * 
+	 * @param canvas
+	 *            The GLCanvas to which to add the listeners.
+	 */
+	public UserInput(GLCanvas canvas) {
+		canvas.addMouseListener(this);
+		canvas.addMouseMotionListener(this);
+		canvas.addKeyListener(this);
+	}
+
+	/*
+	 * **********************************************
+	 * * Updating * **********************************************
+	 */
+
+	@Override
+	public void update() {
+		dX = dx;
+		dY = dy;
+	}
+
+	/*
+	 * **********************************************
+	 * * Input event handlers * **********************************************
+	 */
+
+	@Override
+	public void mousePressed(MouseEvent event) {
+		x = event.getX();
+		y = event.getY();
+		sx = dx;
+		sy = dy;
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent event) {
+		dx = -event.getX() + x + sx;
+		dy = -event.getY() + y + sy;
+		if (dy > 90) {
+			dy = 90;
+		}
+		if (dy < -90) {
+			dy = -90;
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent event) {
+		int key = event.getKeyCode();
+		switch (key) {
+		case KeyEvent.VK_W:
+			forward = true;
+			break;
+		case KeyEvent.VK_A:
+			left = true;
+			break;
+		case KeyEvent.VK_S:
+			back = true;
+			break;
+		case KeyEvent.VK_D:
+			right = true;
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent event) {
+		switch (event.getKeyCode()) {
+		case KeyEvent.VK_W:
+			forward = false;
+			break;
+		case KeyEvent.VK_A:
+			left = false;
+			break;
+		case KeyEvent.VK_S:
+			back = false;
+			break;
+		case KeyEvent.VK_D:
+			right = false;
+			break;
+		}
+	}
+
+	/*
+	 * **********************************************
+	 * * Unused event handlers * **********************************************
+	 */
+
+	@Override
+	public void mouseMoved(MouseEvent event) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent event) {
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent event) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent event) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent event) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent event) {
+	}
+
+}
